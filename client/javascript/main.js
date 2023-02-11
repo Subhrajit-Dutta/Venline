4ea5050a4f551f77e86c38450a415b6313f2a67d
Niladri <rickyrickado14@gmail.com>
2023-02-12 00:35:48 +0530

    added sections

--- client/javascript/main.js
+++ client/javascript/main.js
@@ -1,4 +1,4 @@
-var navbar = document.getElementById('navbar');
+const navbar = document.getElementById('navbar');
 
 function checkscroll(){
     if(window.pageYOffset != 0){
@@ -9,3 +9,31 @@ function checkscroll(){
     }
 }
 window.addEventListener('scroll',checkscroll);
+
+const pcard = document.getElementsByClassName('provide-card');
+const icons = document.getElementsByClassName('i');
+
+function changecolor(){
+    pcard[1].classList.add('cardcolor');
+    pcard[0].classList.remove('cardcolor');
+    for(let i = 0; i < 7; i++){
+        icons[i].classList.remove('logowhite');
+    }
+    for(let i = 7; i < 14; i++){
+        icons[i].classList.add('logowhite');
+    }
+}
+
+function removecolor(){
+    pcard[1].classList.remove('cardcolor');
+    pcard[0].classList.add('cardcolor');
+    for(let i = 0; i < 7; i++){
+        icons[i].classList.add('logowhite');
+    }
+    for(let i = 7; i < 14; i++){
+        icons[i].classList.remove('logowhite');
+    }
+}
+
+pcard[1].addEventListener('mouseover',changecolor);
+pcard[1].addEventListener('mouseout',removecolor);
