a1330fe20f3dce4df393c6895f0ae5387e64f515
Priyanshu <priyanshurouth@gmail.com>
2023-02-13 00:13:26 +0530

    issues fixed

--- server/server.js
+++ server/server.js
@@ -5,15 +5,17 @@ const dotenv= require('dotenv')
 const colors= require("colors")
 const connectDb=require('./config/Database')
 const userRoutes= require("./routes/userRoutes")
+const homeRoutes=require("./routes/Homeroute")
 dotenv.config()
 connectDb()
 const app = express();
 app.use(express.json());
-const PORT = 3000 || process.env.PORT;
+const PORT = 5000 || process.env.PORT;
 const server = http.createServer(app);
-
-// Set static folder
-app.use(express.static(path.join(__dirname, 'client')));
+//sending static path express
+console.log('/client')
+app.use(express.static(path.join(__dirname,'../client')))
+//route for registration credential
 app.use("/api/consumer",userRoutes)
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`.green.bold);
