8f2a0d11ff0412b181fd66fc6d08f6e3ab4e3520
Subhrajit <subhrajitdutta007@gmail.com>
2023-02-13 12:16:31 +0530

    Server semi fixed

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
+
 const app = express();
-app.use(express.json());
 const PORT = 3000 || process.env.PORT;
 const server = http.createServer(app);
-//sending static path express
-app.use(express.static(path.join(__dirname,'/client')))
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'client')));
+
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
