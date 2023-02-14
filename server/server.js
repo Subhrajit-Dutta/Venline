0151e7eca100fa858360ae84aa043a83d7d66960
Priyanshu <priyanshurouth@gmail.com>
2023-02-14 13:38:47 +0530

    Mongo Db Connected Registration Handled

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-
+const dotenv= require('dotenv')
+const connectDb= require('./config/Database')
+const colors= require('colors')
+const userRoutes= require('./routes/userRoutes')
+dotenv.config()
+connectDb()
 const app = express();
+app.use(express.json());
 const PORT = 3000 || process.env.PORT;
 const server = http.createServer(app);
 
@@ -13,7 +19,8 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client', 'index.html'));
 });
-
+//Register user 
+app.use('/api/user',userRoutes)
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`.green.bold);
 });
