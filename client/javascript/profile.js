5d43775323691a26576e3c871fef5736257a4388
Niladri <rickyrickado14@gmail.com>
2023-02-23 21:15:56 +0530

    edit feature now works for profile

--- client/javascript/profile.js
+++ client/javascript/profile.js
@@ -50,6 +50,31 @@ editbtn.addEventListener('click',editShow);
 
 //userdata
 
-const user = JSON.parse(localStorage.getItem('user'));
+let user = JSON.parse(localStorage.getItem('user'));
+
+console.log(localStorage.getItem('user'));
+
 
 document.getElementById('name').innerHTML = `<h1>${user.username}</h1>`;
+
+//edit profile
+
+const nameEdit = document.getElementById('name-edit');
+const phoneEdit = document.getElementById('phone-edit');
+const emailEdit = document.getElementById('email-edit');
+
+nameEdit.value = `${user.username}`;
+phoneEdit.value = `${user.phone}`;
+emailEdit.value = `${user.email}`;
+
+
+const saveBtn = document.getElementById('save');
+
+function saveChanges(event){
+	event.preventDefault();
+	user.username = nameEdit.value;
+	const userUpdated = JSON.stringify(user);
+	localStorage.setItem('user',userUpdated);
+}
+
+saveBtn.addEventListener('click',saveChanges);
