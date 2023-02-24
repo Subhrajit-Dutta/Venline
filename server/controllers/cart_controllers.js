493d98a533f3f1f9c93cc673ca79021f08a8e52a
Priyanshu <priyanshurouth@gmail.com>
2023-02-24 23:12:42 +0530

    updated

--- server/controllers/cart_controllers.js
+++ server/controllers/cart_controllers.js
@@ -2,28 +2,42 @@ const asyncHandler= require('express-async-handler');
 const User=require("../models/ConsumerModel");
 const Cart= require("../models/cartmodel");
 const Product=require("../models/productmodel");
-const addtocart=asyncHandler(async(req,res)=>{
-  const{user,items}=req.body
-  const userid= User.findById(user)
-  if(userid){
-    console.log("user found",user.username)
-    let cartModel = await Cart.findOne({});
-    const product= Product.findById(items.productId)
-    console.log(items.productId)
-    if(product){
-      cartModel.items.push({
-        productId:product._id,
-        quantity:items.quantity,
-        price:price*quantity
-      })
-    }
-    else{
-      res.status(400)
-      throw new Error("Product not found")
-    }
-  await cartModel.save()
-  res.status(200).json({message:"Cart Updated"})
+
+const addToCart = asyncHandler(async(req,res) => {
+  const { user, items } = req.body;
+  let userExists = await User.findOne({ _id: user });
+  if(userExists){
+       
   }
+
+  if (!cartModel) {
+    cartModel = new Cart({ user: userId, items: [] });
+  }
+
+  const cartItem = cartModel.items.find(item => item.productId.toString() === productId);
+
+  if (cartItem) {
+    cartItem.quantity += quantity;
+    cartItem.price += quantity * cartItem.price;
+  } else {
+    const product = await Product.findOne({ _id: productId });
+
+    if (product) {
+      cartModel.items.push({
+        productId: product._id,
+        quantity,
+        price: product.price
+      });
+    } else {
+      res.status(400);
+      throw new Error("Product not found");
+    }
+  }
+
+  await cartModel.save();
+
+  res.status(200).json({ message: "Cart Updated" });
 });
-module.exports={addtocart};
-exports.addtocart=addtocart
+
+module.exports = { addToCart };
+exports.addToCart = addToCart;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const {registerUser,authUser} = require("../controllers/Con_Controller");
-const{addtocart}=require('../controllers/cart_controllers')   
+const{getProducts,getProductById}=require('../controllers/product_controller')
+const{addToCart}=require('../controllers/cart_controllers')   
 const router = express.Router();
 console.log(`registerUser${registerUser}`)
 router.post('/',registerUser);
 router.post('/login',authUser)
-router.post('/cart',addtocart)
+router.post('/cart',addToCart)
+router.post('/product',getProducts)
+router.post('/search',getProductById)
 module.exports = router
